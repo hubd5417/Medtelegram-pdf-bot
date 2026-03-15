42be266b89b3d7d01a10dc0bdc0b069dbeb46e1f
hubd5417 <gduduuhajsjkk@gmail.com>
2026-03-15 10:23:09 +0530

    Update language.py

--- pdf_bot/language.py
+++ pdf_bot/language.py
@@ -2,24 +2,26 @@ import gettext
 
 from telegram import InlineKeyboardButton, InlineKeyboardMarkup
 
-from pdf_bot.constants import USER, LANGUAGE, LANGUAGES
-from pdf_bot.store import client
+from pdf_bot.constants import LANGUAGE, LANGUAGES
+from pdf_bot.store import get_user_data, set_user_language
 
 
 def send_lang(update, context, query=None):
     lang = get_lang(update, context, query)
+
     langs = [
         InlineKeyboardButton(key, callback_data=key)
         for key, value in sorted(LANGUAGES.items(), key=lambda x: x[1])
         if value != lang
     ]
+
     keyboard_size = 2
     keyboard = [
-        langs[i : i + keyboard_size] for i in range(0, len(langs), keyboard_size)
+        langs[i:i + keyboard_size] for i in range(0, len(langs), keyboard_size)
     ]
     reply_markup = InlineKeyboardMarkup(keyboard)
 
-    _ = set_lang(update, context)
+    _ = set_lang(update, context, query)
     update.effective_message.reply_text(
         _("Select your language"), reply_markup=reply_markup
     )
@@ -27,43 +29,41 @@ def send_lang(update, context, query=None):
 
 def get_lang(update, context, query=None):
     if context.user_data is not None and LANGUAGE in context.user_data:
-        lang = context.user_data[LANGUAGE]
+        return context.user_data[LANGUAGE]
+
+    if query is None:
+        user_id = update.effective_message.from_user.id
     else:
-        if query is None:
-            user_id = update.effective_message.from_user.id
-        else:
-            user_id = query.from_user.id
+        user_id = query.from_user.id
 
-        user_key = client.key(USER, user_id)
-        user = client.get(key=user_key)
+    user = get_user_data(user_id)
 
-        if user is None or LANGUAGE not in user:
+    if not user or LANGUAGE not in user:
+        lang = "en_GB"
+    else:
+        lang = user[LANGUAGE]
+        if lang == "en":
             lang = "en_GB"
-        else:
-            lang = user[LANGUAGE]
-            if lang == "en":
-                lang = "en_GB"
 
-        context.user_data[LANGUAGE] = lang
-
+    context.user_data[LANGUAGE] = lang
     return lang
 
 
 def store_lang(update, context, query):
     lang_code = LANGUAGES[query.data]
-    with client.transaction():
-        user_key = client.key(USER, query.from_user.id)
-        user = client.get(key=user_key)
-        user[LANGUAGE] = lang_code
-        client.put(user)
+    set_user_language(query.from_user.id, lang_code)
 
     context.user_data[LANGUAGE] = lang_code
-    _ = set_lang(update, context)
+    _ = set_lang(update, context, query)
     query.message.edit_text(_("Your language has been set to {}").format(query.data))
 
 
 def set_lang(update, context, query=None):
     lang = get_lang(update, context, query)
-    t = gettext.translation("pdf_bot", localedir="locale", languages=[lang])
-
+    t = gettext.translation(
+        "pdf_bot",
+        localedir="locale",
+        languages=[lang],
+        fallback=True,
+    )
     return t.gettext
